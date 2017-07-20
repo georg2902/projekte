633ff7572605c5c27f4b1fd9e0e84e88d6727601
georg <georg2902@gmx.de>
2017-07-21 01:01:23 +0200

    rgb2cmyk and pics

--- Uebungen/src/RGB2CMYK.java
+++ Uebungen/src/RGB2CMYK.java
@@ -7,6 +7,7 @@ import org.opencv.imgproc.Imgproc;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Vector;
 
 /**
@@ -19,33 +20,73 @@ public class RGB2CMYK {
         System.out.println(new RGB2CMYK().getClass().getResource("C:/git/projekte/Uebungen/src/background.jpg").getPath());
         Mat src = Imgcodecs.imread(new RGB2CMYK().getClass().getResource("/background.jpg").getPath());
         ArrayList<Mat> dst = new ArrayList<>();
-        new RGB2CMYK().r2c(src,dst);
+        new RGB2CMYK().r2c(src, dst);
 
 
     }
-    public void r2c(Mat img, ArrayList<Mat> cmyk){
+
+    public void r2c(Mat img, ArrayList<Mat> cmyk) {
         for (int i = 0; i < 4; i++) {
             cmyk.add(new Mat(img.size(), CvType.CV_8UC1));
         }
         //get rgb
         ArrayList<Mat> rgb = new ArrayList<>();
-        Core.split(img,rgb);
+        ArrayList<Double> averageC = new ArrayList<>();
+        ArrayList<Double> averageM = new ArrayList<>();
+        ArrayList<Double> averageY = new ArrayList<>();
+        ArrayList<Double> averageK = new ArrayList<>();
+        Core.split(img, rgb);
 
         //rgb to cmyk
+
         for (int i = 0; i < img.rows(); i++) {
             for (int j = 0; j < img.cols(); j++) {
-                float r = (int) rgb.get(0).get(i,j)[0] / 255;
-                float g = (int) rgb.get(1).get(i,j)[1] / 255;
-                float b = (int) rgb.get(2).get(i,j)[2] / 255;
 
-                float k = Math.min(Math.min(1-r,1-g),1-b);
-
-                cmyk.get(0).get(i,j)[0] = (1-r-k) / (1-k) * 255;
-                cmyk.get(1).get(i,j)[1] = (1-g-k) / (1-k) * 255;
-                cmyk.get(2).get(i,j)[2] = (1-b-k) / (1-k) * 255;
-                cmyk.get(3).get(i,j)[3] = k*255;
+                float r = (int) rgb.get(0).get(i, j)[0] / 255.0F;
+                // System.out.println(r);
+                float g = (int) rgb.get(1).get(i, j)[0] / 255.0F;
+                //System.out.println(g);
+                float b = (int) rgb.get(2).get(i, j)[0] / 255.0F;
+                //System.out.println(b);
+                float k = Math.min(Math.min(1 - r, 1 - g), 1 - b);
+                //System.out.println(k);
+                double cValue = (1 - r - k) / (1 - k);
+                double mValue = (1 - g - k) / (1 - k);
+                double yValue = (1 - b - k) / (1 - k);
+                double kValue = k;
+                // System.out.println("________");
+                //System.out.println("C: "+cValue);
+                //System.out.println("M: "+mValue);
+                // System.out.println("Y: "+yValue);
+                //System.out.println("K: "+kValue);
+                averageC.add(cValue);
+                averageM.add(mValue);
+                averageY.add(yValue);
+                averageK.add(kValue);
+                cmyk.get(0).put(i, j, yValue * 255.0F);
+                cmyk.get(1).put(i, j, mValue * 255.0F);
+                cmyk.get(2).put(i, j, cValue * 255.0F);
+                cmyk.get(3).put(i, j, kValue * 255.0F);
+                //cmyk.get(0).get(i, j)[0] = (1 - r - k) / (1 - k) * 255.0F;
+                //cmyk.get(1).get(i, j)[0] = (1 - g - k) / (1 - k) * 255.0F;
+                //cmyk.get(2).get(i, j)[0] = (1 - b - k) / (1 - k) * 255.0F;
+                //cmyk.get(3).get(i, j)[0] = k * 255.0F;
             }
         }
 
+        double sumC = averageC.stream().mapToDouble(Double::doubleValue).sum();
+        double sumM = averageM.stream().mapToDouble(Double::doubleValue).sum();
+        double sumY = averageY.stream().mapToDouble(Double::doubleValue).sum();
+        double sumK = averageK.stream().mapToDouble(Double::doubleValue).sum();
+
+        double averageCValue = sumC / averageC.size();
+        double averageMValue = sumM / averageM.size();
+        double averageYValue = sumY / averageY.size();
+        double averageKValue = sumK / averageK.size();
+
+        System.out.println("C: " + averageYValue);
+        System.out.println("M: " + averageMValue);
+        System.out.println("Y: " + averageCValue);
+        System.out.println("K: " + averageKValue);
     }
 }
--- Uebungen/src/RGB2CMYKMain.java
+++ Uebungen/src/RGB2CMYKMain.java
@@ -1,7 +1,13 @@
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 /**
@@ -10,11 +16,56 @@ import java.util.ArrayList;
 public class RGB2CMYKMain {
     public static void main(String[] args) {
         RGB2CMYK test = new RGB2CMYK();
-        File x = new File("src/background.jpg");
-        System.out.println("x.getAbsolutePath() = " + x.getAbsolutePath());
-        System.out.println("x.exists() = " + x.exists());
-        Mat src = Imgcodecs.imread(x.getPath());
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat src = Imgcodecs.imread("src/blut1hcropped.jpg");
         ArrayList<Mat> dst = new ArrayList<>();
-        test.r2c(src,dst);
+        test.r2c(src, dst);
+        BufferedImage img1 = Mat2BufferedImage(dst.get(0));
+        BufferedImage img2 = Mat2BufferedImage(dst.get(1));
+        BufferedImage img3 = Mat2BufferedImage(dst.get(2));
+        BufferedImage img4 = Mat2BufferedImage(dst.get(3));
+        BufferedImage sourceImg = Mat2BufferedImage(src);
+        displayImage(sourceImg, "source");
+        displayImage(img1, "C");
+        displayImage(img2,"M");
+        displayImage(img3,"Y");
+        displayImage(img4,"K");
+
+
     }
+
+    public static BufferedImage Mat2BufferedImage(Mat m) {
+// source: http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/
+// Fastest code
+// The output can be assigned either to a BufferedImage or to an Image
+
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (m.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = m.channels() * m.cols() * m.rows();
+        byte[] b = new byte[bufferSize];
+        m.get(0, 0, b); // get all the pixels
+        BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(b, 0, targetPixels, 0, b.length);
+        return image;
+    }
+
+    public static void displayImage(Image img2, String title)
+    {
+        //BufferedImage img=ImageIO.read(new File("/HelloOpenCV/lena.png"));
+        ImageIcon icon=new ImageIcon(img2);
+        JFrame frame=new JFrame();
+        frame.setTitle(title);
+        frame.setLayout(new FlowLayout());
+        frame.setSize(img2.getWidth(null)+50, img2.getHeight(null)+50);
+        JLabel lbl=new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
 }
